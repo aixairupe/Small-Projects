6a3b948e98bec3f5f36f470d1ea32affc66d3185
aixairupe <aixairupe992@gmail.com>
2022-11-17 20:27:09 -0300

    madlibs initial commit

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adjetivo=input("Ingrese un adjetivo: ")
+sustantivo1=input("Ingrese un sustantivo: ")
+sustantivo2=input("Ingrese otro sustantivo")
+sustantivo3=input("Ingrese otro sustantivo")
+sustantivo_plural=input("Ingrese un sustantivo en plural: ")
+
+print(f"La noche estaba {adjetivo}. Entre las nubes, las estrellas parecían {sustantivo1} en el cielo. De pronto, frente a mí apareció un {sustantivo2} que me asustó hasta la médula. Corrí hacia la {sustantivo3} a refugiarme pero me detuve antes de abrir la puerta. Del otro lado se escuchaban {sustantivo_plural}.")
